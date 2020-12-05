8c671572690000028f323bedfb21a8f92bacf54b
Luuk van Heumen <luukvheumen@gmail.com>
2020-12-05 23:34:49 +0100

    Reformat code

--- tictactoe.py
+++ tictactoe.py
@@ -6,17 +6,15 @@
 
 # As this implementation uses distinct cells that can be changed, but shouldn't allow for duplicates, a dictionary is appropriate to use here.
 # In this case, every key will initially be linked to a string indicating board position
-boardState = {'1':'1', '2':'2', '3':'3',
-              '4':'4', '5':'5', '6':'6',
-              '7':'7', '8':'8', '9':'9'}
 
+boardState = {str(x):str(x) for x in range(1,10)}
 # This implementation requires the board to constantly be redrawn, so this will be relegated to a function
 def drawBoard(board):
-    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+    print(f"{board['1']}|{board['2']}|{board['3']}")
     print('-+-+-')
-    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print(f"{board['4']}|{board['5']}|{board['6']}")
     print('-+-+-')
-    print(board['7'] + '|' + board['8'] + '|' + board['9'])
+    print(f"{board['7']}|{board['8']}|{board['9']}")
 
 drawBoard(boardState)
 
@@ -26,13 +24,13 @@ player_2 = 'O'
 activePlayer = player_1
 
 for i in range(1,10):
-    placeAvailable = 0
-    print('Player ' + activePlayer + ', where would you like to place your symbol?')
+    placeAvailable = False
+    print(f'Player {activePlayer}, where would you like to place your symbol?')
 
-    while placeAvailable == 0:
+    while placeAvailable == False:
         move = input()
-        if move in boardState.values() and move != (player_1 or player_2) :                                         # Opting for values as the keys don't change, so they can be overwritten in theory
-            placeAvailable = 1
+        if move in boardState.values() and move != (player_1 or player_2):                                         # Opting for values as the keys don't change, so they can be overwritten in theory
+            placeAvailable = True
             boardState[move] = activePlayer
         else:
             print('That spot is not available, please select another')
@@ -40,6 +38,4 @@ for i in range(1,10):
     drawBoard(boardState)
 
     # Player swap
-    if activePlayer == player_1:
-        activePlayer = player_2
-    else: activePlayer = player_1
+    activePlayer = player_2 if activePlayer == player_1 else player_1
