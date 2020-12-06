7e6e16950da119aa122ae88827f93b10ac34905b
Luuk van Heumen <luukvheumen@gmail.com>
2020-12-06 15:08:19 +0100

    Add win/tie conditions

--- tictactoe.py
+++ tictactoe.py
@@ -4,10 +4,6 @@
    Several of these integrations exist, often using the numpad on a full-size
    keyboard. This version however is made with TKL in mind.'''
 
-# As this implementation uses distinct cells that can be changed, but shouldn't allow for duplicates, a dictionary is appropriate to use here.
-# In this case, every key will initially be linked to a string indicating board position
-
-boardState = {str(x):str(x) for x in range(1,10)}
 # This implementation requires the board to constantly be redrawn, so this will be relegated to a function
 def drawBoard(board):
     print(f"{board['1']}|{board['2']}|{board['3']}")
@@ -16,26 +12,67 @@ def drawBoard(board):
     print('-+-+-')
     print(f"{board['7']}|{board['8']}|{board['9']}")
 
-drawBoard(boardState)
+# Function to print the game winner
+def wonGame(player,board):
+    drawBoard(board)
+    print(f'\n******** Player {player} has won, congratulations! ********\n')
 
+# Define playing board
+boardState = {str(x):str(x) for x in range(1,10)}
+
+# Define player names
 player_1 = 'X'
 player_2 = 'O'
 
+# Set active player
 activePlayer = player_1
 
+# The game itself
 for i in range(1,10):
+
+    drawBoard(boardState)
     placeAvailable = False
     print(f'Player {activePlayer}, where would you like to place your symbol?')
 
     while placeAvailable == False:
         move = input()
-        if move in boardState.values() and move != (player_1 or player_2):                                         # Opting for values as the keys don't change, so they can be overwritten in theory
+        if move in boardState.values() and move != (player_1 or player_2):
             placeAvailable = True
             boardState[move] = activePlayer
         else:
             print('That spot is not available, please select another')
 
-    drawBoard(boardState)
+    # Checking win conditions on the 5th turn and thereafter
+    # Tic-tac-toe has 8 discrete win states, all will be checked
+    if i >= 5:
+        if boardState['1'] == boardState['2'] == boardState['3']:
+            wonGame(activePlayer,boardState)
+            break
+        elif boardState['4'] == boardState['5'] == boardState['6']:
+            wonGame(activePlayer,boardState)
+            break
+        elif boardState['7'] == boardState['8'] == boardState['9']:
+            wonGame(activePlayer,boardState)
+            break
+        elif boardState['1'] == boardState['4'] == boardState['7']:
+            wonGame(activePlayer,boardState)
+            break
+        elif boardState['2'] == boardState['5'] == boardState['8']:
+            wonGame(activePlayer,boardState)
+            break
+        elif boardState['3'] == boardState['6'] == boardState['9']:
+            wonGame(activePlayer,boardState)
+            break
+        elif boardState['1'] == boardState['5'] == boardState['9']:
+            wonGame(activePlayer,boardState)
+            break
+        elif boardState['3'] == boardState['5'] == boardState['7']:
+            wonGame(activePlayer,boardState)
+            break
 
+    # If nobody wins after the 9th token, the game is a tie
+    if i == 9:
+        drawBoard(boardState)
+        print(f'\nGame Over!\nIt\'s a tie!')
     # Player swap
     activePlayer = player_2 if activePlayer == player_1 else player_1
