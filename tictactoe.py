2d2c4eedd2933bda5b1ee2264ac0762e189a7c4d
Luuk van Heumen <luukvheumen@gmail.com>
2020-12-06 18:42:36 +0100

    Refactor code add restart option

--- tictactoe.py
+++ tictactoe.py
@@ -6,73 +6,87 @@
 
 # This implementation requires the board to constantly be redrawn, so this will be relegated to a function
 def drawBoard(board):
-    print(f"{board['1']}|{board['2']}|{board['3']}")
-    print('-+-+-')
-    print(f"{board['4']}|{board['5']}|{board['6']}")
-    print('-+-+-')
-    print(f"{board['7']}|{board['8']}|{board['9']}")
+    print(f"{board['1']}│{board['2']}│{board['3']}")
+    print('─┼─┼─')
+    print(f"{board['4']}│{board['5']}│{board['6']}")
+    print('─┼─┼─')
+    print(f"{board['7']}│{board['8']}│{board['9']}")
 
 # Function to print the game winner
 def wonGame(player,board):
     drawBoard(board)
     print(f'\n******** Player {player} has won, congratulations! ********\n')
 
-# Define playing board
-boardState = {str(x):str(x) for x in range(1,10)}
+# Define the game itself as a Function
+def playGame():
+    # Define playing board
+    boardState = {str(x):str(x) for x in range(1,10)}
 
-# Define player names
-player_1 = 'X'
-player_2 = 'O'
+    # Define player names
+    player_1 = 'X'
+    player_2 = 'O'
 
-# Set active player
-activePlayer = player_1
+    # Set active player
+    activePlayer = player_1
 
-# The game itself
-for i in range(1,10):
+    for turn in range(1,10):
 
-    drawBoard(boardState)
-    placeAvailable = False
-    print(f'Player {activePlayer}, where would you like to place your symbol?')
-
-    while placeAvailable == False:
-        move = input()
-        if move in boardState.values() and move != (player_1 or player_2):
-            placeAvailable = True
-            boardState[move] = activePlayer
-        else:
-            print('That spot is not available, please select another')
-
-    # Checking win conditions on the 5th turn and thereafter
-    # Tic-tac-toe has 8 discrete win states, all will be checked
-    if i >= 5:
-        if boardState['1'] == boardState['2'] == boardState['3']:
-            wonGame(activePlayer,boardState)
-            break
-        elif boardState['4'] == boardState['5'] == boardState['6']:
-            wonGame(activePlayer,boardState)
-            break
-        elif boardState['7'] == boardState['8'] == boardState['9']:
-            wonGame(activePlayer,boardState)
-            break
-        elif boardState['1'] == boardState['4'] == boardState['7']:
-            wonGame(activePlayer,boardState)
-            break
-        elif boardState['2'] == boardState['5'] == boardState['8']:
-            wonGame(activePlayer,boardState)
-            break
-        elif boardState['3'] == boardState['6'] == boardState['9']:
-            wonGame(activePlayer,boardState)
-            break
-        elif boardState['1'] == boardState['5'] == boardState['9']:
-            wonGame(activePlayer,boardState)
-            break
-        elif boardState['3'] == boardState['5'] == boardState['7']:
-            wonGame(activePlayer,boardState)
-            break
-
-    # If nobody wins after the 9th token, the game is a tie
-    if i == 9:
         drawBoard(boardState)
-        print(f'\nGame Over!\nIt\'s a tie!')
-    # Player swap
-    activePlayer = player_2 if activePlayer == player_1 else player_1
+        placeAvailable = False
+        print(f'Player {activePlayer}, where would you like to place your symbol?')
+
+        while placeAvailable == False:
+            move = input()
+            if move in boardState.values() and move != (player_1 or player_2):
+                placeAvailable = True
+                boardState[move] = activePlayer
+            else:
+                print('That spot is not available, please select another')
+
+        # Checking win conditions on the 5th turn and thereafter
+        # Tic-tac-toe has 8 discrete win states, all will be checked
+        if turn >= 5:
+            if boardState['1'] == boardState['2'] == boardState['3']:
+                wonGame(activePlayer,boardState)
+                break
+            elif boardState['4'] == boardState['5'] == boardState['6']:
+                wonGame(activePlayer,boardState)
+                break
+            elif boardState['7'] == boardState['8'] == boardState['9']:
+                wonGame(activePlayer,boardState)
+                break
+            elif boardState['1'] == boardState['4'] == boardState['7']:
+                wonGame(activePlayer,boardState)
+                break
+            elif boardState['2'] == boardState['5'] == boardState['8']:
+                wonGame(activePlayer,boardState)
+                break
+            elif boardState['3'] == boardState['6'] == boardState['9']:
+                wonGame(activePlayer,boardState)
+                break
+            elif boardState['1'] == boardState['5'] == boardState['9']:
+                wonGame(activePlayer,boardState)
+                break
+            elif boardState['3'] == boardState['5'] == boardState['7']:
+                wonGame(activePlayer,boardState)
+                break
+
+        # If nobody wins after the 9th token, the game is a tie
+        if turn == 9:
+            drawBoard(boardState)
+            print(f'\nGame Over!\nIt\'s a tie!')
+            break
+        # Player swap
+        activePlayer = player_2 if activePlayer == player_1 else player_1
+
+    # Ask for a restart
+    print('Would you like to restart? y/n')
+    restartGame = input()
+
+    print()
+
+    if restartGame == 'y' or restartGame == 'Y':
+        print('**** Restarting ****')
+        playGame()
+
+playGame()
